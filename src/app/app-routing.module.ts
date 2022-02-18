be4fac90cef0b978eef6cf67c931a9c12f1b8a13
Alejandra244 <alejandrass2003@outlook.es>
2022-02-18 18:01:23 -0400

    Routing y lista clientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearClienteComponent } from './components/crear-cliente/crear-cliente.component';
+import { ListaClientesComponent } from './components/lista-clientes/lista-clientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo: "lista-clientes", pathMatch:"full"},
+  { path:"lista-clientes", component: ListaClientesComponent},
+  {path:"crear-cliente", component: CrearClienteComponent},
+  {path:"**", redirectTo: "lista-clientes", pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
