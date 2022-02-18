599c1a980a29c9810e1817c51cf68fd9d7fc2507
Alejandra244 <alejandrass2003@outlook.es>
2022-02-18 14:55:37 -0400

    navbar lindo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaClientesComponent } from './components/lista-clientes/lista-clientes.component';
+import { CrearClienteComponent } from './components/crear-cliente/crear-cliente.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaClientesComponent,
+    CrearClienteComponent,
+    NavbarComponent,
+    
+   
   ],
   imports: [
     BrowserModule,
